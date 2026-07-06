1d35632ecf464f8c4137d17358088822d7f790d8
evea12 <evelyn.e.agren@gmail.com>
2026-07-06 16:01:05 +0200

    Update map.js

--- 0/map.js
+++ 0/map.js
@@ -10,7 +10,7 @@ if (!mapboxgl.supported()) alert("Your browser does not support Mapbox GL");
 mapboxgl.accessToken = "pk.eyJ1IjoiZXZlYTEyIiwiYSI6ImNtcjdzYXY5MTBocnEyeXFvYTRqamo4YTUifQ.xzMb4LxFvFWK7NVWI_tNLg";
 map = new mapboxgl.Map({
     container: "map",
-    style: 'mapbox://styles/mapbox/satellite-streets-v9',
+    style: 'mapbox://styles/mapbox/standard',
     // style: "https://openmaptiles.github.io/dark-matter-gl-style/style-cdn.json",
     // style: "https://openmaptiles.github.io/positron-gl-style/style-cdn.json",
     // style: "https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json",            
