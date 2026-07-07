9eabf71db98e2b574ddd6ea407bf691c75c1c33a
evea12 <evelyn.e.agren@gmail.com>
2026-07-07 22:56:23 +0200

    Update map2.js

--- 0/map2.js
+++ 0/map2.js
@@ -6,7 +6,7 @@ var playable = true;
 initAudio();
 
 //10.15332,56.16728
-const testa = [9.97038888, 53.55922]
+const testa = [9.97038888, 53.55922];
 const train = [9.97684,53.55819];
 const wind= [9.97920,53.56170];
 const map = new mapboxgl.Map({
