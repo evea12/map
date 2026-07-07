91397a7a840e9e70e742a600286727274958ac48
evea12 <evelyn.e.agren@gmail.com>
2026-07-07 21:25:04 +0200

    Update map2.js

--- 0/map2.js
+++ 0/map2.js
@@ -72,11 +72,9 @@ map.addControl(geolocate);
 // Set an event listener that fires
 // when a geolocate event occurs.
 geolocate.on('geolocate', function (e) {
-    console.log("Geolocated: " + e.coords.longitude + "," + e.coords.latitude);
     var position = [e.coords.longitude, e.coords.latitude];
     var units = { units: "meters" };
     var distancetrain = turf.distance(position, train, units);
-    var distancewind = turf.distance(position, wind, units);
     if (distancetrain < 50){const popuptrain = new mapboxgl.Popup({ offset: 25 }).setHTML('<h3>' + "U-Bahn" + '</h3><p>' + "Recorded 7.7.26 in the Messehallen U-Bahn station" + '</p>' +
             '<audio controls autoplay><source src="' + "Train.mp3" + '" type="audio/mpeg"></audio>');const eltrain = document.createElement('div');
     eltrain.id = 'marker';new mapboxgl.Marker(eltrain)
